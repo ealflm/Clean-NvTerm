120a6fdc9ecde98c3274cae4794ea939306d6b7d
NamDP <ealflm@gmail.com>
2024-03-15 10:08:43 +0700

    commit

---
+++ kill_nvterms.bat
@@ -0,0 +1,15 @@
+@echo off
+
+for /f "tokens=*" %%a in (C:\Users\ealflm\.nvterm_info) do (
+    for /f "tokens=1" %%b in ('tasklist /FI "PID eq %%a" /NH') do (
+        REM echo %%b
+        if "%%b"=="pwsh.exe" (
+            REM echo Process %%a is running PowerShell.
+            taskkill /F /PID %%a
+        ) else (
+            REM echo Process %%a is not running PowerShell.
+        )
+    )
+)
+
+del "C:\Users\ealflm\.nvterm_info"
